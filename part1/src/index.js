567a6a72b389baca23956c387d5445709df4f8fc
lagp <luisalfonsogp@gmail.com>
2023-08-19 21:19:31 +0200

    Part1 - Exercises 1.7

--- part1/src/index.js
+++ part1/src/index.js
@@ -6,11 +6,17 @@ const Title = ({text}) => <h1>{text}</h1>
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
 const Statics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const averageScore = (good * 1 + neutral * 0 + bad * -1) / total
+  const positivePercent = (good / total) * 100
 return <>
       <Title text='statics' />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {!isNaN(averageScore) ? averageScore : 0}</p>
+      <p>positive {!isNaN(positivePercent) ? positivePercent : 0} %</p>
 </>
 }
 
